a58f6a81080f49eb6540640e9d4bb7dad566b052
Marcio Almeida <marcioandredasilvaalmeida@gmail.com>
2017-09-17 19:01:46 -0300

    hjh

--- style-atual-webService/src/br/com/styleAtual/model/domain/Vendedor.java
+++ style-atual-webService/src/br/com/styleAtual/model/domain/Vendedor.java
@@ -2,4 +2,13 @@ package br.com.styleAtual.model.domain;
 
 public class Vendedor {
 
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
