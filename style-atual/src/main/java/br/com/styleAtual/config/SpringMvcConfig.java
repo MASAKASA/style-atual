d30ad3ab6377c1a5fe1de096ab0948676b61f172
Anderson Vasconcelos <anderson_vasconcelos2010@hotmail.com>
2017-09-26 12:48:50 -0300

    Alterações no pacote Domain (Cliente, Produto, Caixa, Movimento)

--- style-atual/src/main/java/br/com/styleAtual/config/SpringMvcConfig.java
+++ style-atual/src/main/java/br/com/styleAtual/config/SpringMvcConfig.java
@@ -13,7 +13,7 @@ public class SpringMvcConfig {
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		//PREXIXO ONDE O SPRING VAI PROCURAR AS PÁGINAS
-		resolver.setPrefix("WEB-INF/views/");
+		resolver.setPrefix("/WEB-INF/views/");
 		//TIPO DE ARQUIVO DA PÁGINA
 		resolver.setSuffix(".jsp");
 		//INFORMANDO QUAL TIPO DE RECURSO SERÃO USADOS NA PÁGINA JSP
