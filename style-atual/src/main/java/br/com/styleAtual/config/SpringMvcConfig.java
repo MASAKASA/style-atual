8abc67d01c45f6fd703e9b0e9c205d223a3b0feb
Anderson Vasconcelos <anderson_vasconcelos2010@hotmail.com>
2017-09-26 00:37:43 -0300

    Alteração de controller Produto

--- style-atual/src/main/java/br/com/styleAtual/config/SpringMvcConfig.java
+++ style-atual/src/main/java/br/com/styleAtual/config/SpringMvcConfig.java
@@ -8,11 +8,15 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 public class SpringMvcConfig {
 
+	//ANOTAÇÃO PARA O SPRING CONTROLAR O MÉTODO
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		//PREXIXO ONDE O SPRING VAI PROCURAR AS PÁGINAS
 		resolver.setPrefix("WEB-INF/views/");
+		//TIPO DE ARQUIVO DA PÁGINA
 		resolver.setSuffix(".jsp");
+		//INFORMANDO QUAL TIPO DE RECURSO SERÃO USADOS NA PÁGINA JSP
 		resolver.setViewClass(JstlView.class);
 		return resolver;
 	}
