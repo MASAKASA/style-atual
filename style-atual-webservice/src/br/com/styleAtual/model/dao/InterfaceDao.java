f1b59e487adca7478fb19186175c181612289b14
Marcio Almeida <marcioandredasilvaalmeida@gmail.com>
2017-11-02 10:58:08 -0300

    Comentarios na interface InterfaceDao

--- style-atual-webservice/src/br/com/styleAtual/model/dao/InterfaceDao.java
+++ style-atual-webservice/src/br/com/styleAtual/model/dao/InterfaceDao.java
@@ -6,71 +6,69 @@ import java.util.List;
  * 
  * @author MARCIO
  *
- * @param <O>
- * @param <I>
- * @param <S>
- * @param <L>
+ * @param <O> Objeto o qual vai ser realizado as transações com o banco de dados 
+ * @param <I> Vão ser dois valor int para pesquisa com paginação: firsResult para primeiro
+ *  resultado e maxResult para quantidade de retorno
+ * @param <S> String para pesquisa por nome do objeto
+ * @param <L> Long para pesquisa por chave primaria do objeto
  */
 public interface InterfaceDao <O,I,S,L>{
 
 	/**
 	 * 
-	 * @param object
-	 * @return
+	 * @param Object para ser salvo no banco de dados
 	 */
 	public void save(O object);
 	
 	/**
 	 * 
-	 * @param object
-	 * @return
+	 * @param Object para ser atualizado no banco de dados
 	 */
 	public void update(O object);
 	
 	/**
 	 * 
-	 * @param id
-	 * @return
+	 * @param Long id do objeto para realização de delete no banco de dados 
 	 * @throws DAOException
 	 */
 	public void delete(L id);// throws DAOException;
 	
 	/**
 	 * 
-	 * @param id
-	 * @return
+	 * @param Long id do objeto para pesquisa no banco de dados 
+	 * @return Objeto da pesquisa no banco de dados
 	 * @throws DAOException
 	 */
 	public O getById(L id);// throws DAOException;
 	
 	/**
 	 * 
-	 * @param firsResult
-	 * @param maxResult
-	 * @return
+	 * @param Integer firsResult para primeiro resultado da pesquisa no banco de dados
+	 * @param Integer maxResult para total de resultado da pesquisa no banco de dados 
+	 * @return Lista com objetos da pesquisa no banco e dados
 	 * @throws DAOException
 	 */
 	public List<O> getByPagination(I firsResult, I maxResult);// throws DAOException;
 	
 	/**
 	 * 
-	 * @param name
-	 * @return
+	 * @param String com o nome do objeto para pesquisa no banco de dados 
+	 * @return Lista com objetos da pesquisa no banco e dados
 	 * @throws DAOException
 	 */
 	public List<O> getByName(S name);// throws DAOException;
 	
 	/**
 	 * 
-	 * @return
+	 * @return Lista com todos objetos da pesquisa no banco e dados
 	 * @throws DAOException
 	 */
 	public List<O> getAll();// throws DAOException;
 	
 	/**
 	 * 
-	 * @param object
-	 * @return
+	 * @param Object para saber se existe no banco de dados
+	 * @return False se o objeto não existe e true se existe no banco de dados
 	 */
 	public Boolean objectIsValid(O object);
 }
