023846ec051a5287dc89c371dbf3369a9460aa38
Marcio Almeida <marcioandredasilvaalmeida@gmail.com>
2017-11-06 00:19:30 -0300

    Implementando os codigos de respostas HTTP certas para cada metodo

--- style-atual-webservice/src/br/com/styleAtual/controller/InterfaceController.java
+++ style-atual-webservice/src/br/com/styleAtual/controller/InterfaceController.java
@@ -2,13 +2,7 @@ package br.com.styleAtual.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.ResponseEntity;
 
 /**
  * 
@@ -26,26 +20,26 @@ public interface InterfaceController <O,I,S,L>{
 	 * 
 	 * @param Object para ser salvo no banco de dados 
 	 */
-	public O save(O object);
+	public ResponseEntity<O> save(O object);
 	
 	/**
 	 * 
 	 * @param Object para ser atualizado no banco de dados
 	 */
-	public O update(O object);
+	public ResponseEntity<O> update(O object);
 	
 	/**
 	 * 
 	 * @param Long id do objeto para realização de delete no banco de dados 
 	 */
-	public S delete(L id);
+	public ResponseEntity<O> delete(L id);
 	
 	/**
 	 * 
 	 * @param Long id do objeto para pesquisa no banco de dados 
 	 * @return Objeto da pesquisa no banco de dados
 	 */
-	public O getById(L id);
+	public ResponseEntity<O> getById(L id);
 	
 	/**
 	 * 
@@ -53,18 +47,18 @@ public interface InterfaceController <O,I,S,L>{
 	 * @param Integer maxResult para total de resultado da pesquisa no banco de dados 
 	 * @return Lista com objetos da pesquisa no banco e dados
 	 */
-	public List<O> getByPagination(I firsResult, I maxResult);
+	public ResponseEntity<List<O>> getByPagination(I firsResult, I maxResult);
 	
 	/**
 	 * 
 	 * @param String com o nome do objeto para pesquisa no banco de dados 
 	 * @return Lista com objetos da pesquisa no banco e dados
 	 */
-	public List<O> getByName(S name);
+	public ResponseEntity<List<O>> getByName(S name);
 	
 	/**
 	 * 
 	 * @return Lista com todos objetos da pesquisa no banco e dados
 	 */
-	public List<O> getAll();
+	public ResponseEntity<List<O>> getAll();
 }
--- style-atual-webservice/src/br/com/styleAtual/controller/VendedorController.java
+++ style-atual-webservice/src/br/com/styleAtual/controller/VendedorController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +19,7 @@ import br.com.styleAtual.model.domain.Vendedor;
 import br.com.styleAtual.service.InterfaceService;
 
 @Controller //anotacao informando que essa classe vai ser da camada de controle
-@RequestMapping("vendedor") //Mapiamneto Rest para o Spring poder encontrar o controlador certo[http://localhost:8080/style-atual-webservice/vendedor] 
+@RequestMapping("vendedor") //Mapiamneto Rest para o Spring poder encontrar o controlador certo[http://localhost:8080/style-atual-webservice/vendedor]
 public class VendedorController implements InterfaceController<Vendedor, Integer, String, Long>{
 
 	@Autowired //Injecao de depentencia
@@ -25,47 +27,48 @@ public class VendedorController implements InterfaceController<Vendedor, Integer
 	
 	@Override
 	@RequestMapping(value = "/save", method = RequestMethod.POST) //Mapiamneto Rest para o Spring poder encontrar o metodo no certo[http://localhost:8080/style-atual-webservice/vendedor/save]. Tambem informando que a requisicao vai ser via POST
-	public @ResponseBody Vendedor save(@Valid @RequestBody Vendedor vendedor) { //@ResponseBody para informar que o envio e o retorno vai em SJon no Body. @Valid para que o Hibernate-validator valida o objeto em relacao as anotacoes colocadas na entidade
+	public ResponseEntity<Vendedor> save(@Valid @RequestBody Vendedor vendedor) { //@ResponseBody para informar que o envio e o retorno vai em SJon no Body. @Valid para que o Hibernate-validator valida o objeto em relacao as anotacoes colocadas na entidade
 		vendedorService.save(vendedor);
-		return vendedor;
+		return new ResponseEntity<Vendedor>(vendedor, HttpStatus.CREATED);
 	}
 
 	@Override
 	@RequestMapping(value = "/update" , method = RequestMethod.PUT)//Mapiamneto Rest para o Spring poder encontrar o metodo no certo[http://localhost:8080/style-atual-webservice/vendedor/update]. Tambem informando que a requisicao vai ser via PUT
-	public @ResponseBody Vendedor update(@Valid @RequestBody Vendedor vendedor) {//@ResponseBody para informar que o  envio e o retorno vai em SJon no Body. @Valid para que o Hibernate-validator valida o objeto em relacao as anotacoes colocadas na entidade
+	public ResponseEntity<Vendedor> update(@Valid @RequestBody Vendedor vendedor) {//@ResponseBody para informar que o  envio e o retorno vai em SJon no Body. @Valid para que o Hibernate-validator valida o objeto em relacao as anotacoes colocadas na entidade
 		vendedorService.update(vendedor);
-		return vendedor;
+		return new ResponseEntity<Vendedor>(vendedor, HttpStatus.NO_CONTENT);
 	}
 
 	@Override
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)//Mapiamneto Rest para o Spring poder encontrar o metodo no certo com parametro na requisicao[http://localhost:8080/style-atual-webservice/vendedor/delete/{id}]. Tambem informando que a requisicao vai ser via DELETE
-	public @ResponseBody String delete(@PathVariable("id") Long id) {//@ResponseBody para informar que o retorno vai em SJon no Body. @PathVariable Parametro da requisicao REST
-		return vendedorService.delete(id);
+	public ResponseEntity<Vendedor> delete(@PathVariable("id") Long id) {//@ResponseBody para informar que o retorno vai em SJon no Body. @PathVariable Parametro da requisicao REST
+		vendedorService.delete(id);
+		return new ResponseEntity<Vendedor>(HttpStatus.NO_CONTENT);
 	}
 
 	@Override
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)//Mapiamneto Rest para o Spring poder encontrar o metodo no certo com parametro na requisicao[http://localhost:8080/style-atual-webservice/vendedor/{id}]. Tambem informando que a requisicao vai ser via GET
-	public @ResponseBody Vendedor getById(@PathVariable("id") Long id) {//@ResponseBody para informar que o retorno vai em SJon no Body. @PathVariable Parametro da requisicao REST
-		return vendedorService.getById(id);
+	public ResponseEntity<Vendedor> getById(@PathVariable("id") Long id) {//@ResponseBody para informar que o retorno vai em SJon no Body. @PathVariable Parametro da requisicao REST
+		return new ResponseEntity<Vendedor>(vendedorService.getById(id), HttpStatus.OK);
 	}
 
 	@Override
 	@RequestMapping(value = "/pagination", method = RequestMethod.GET)//Mapiamneto Rest para o Spring poder encontrar o metodo no certo com dois parametros na requisicao[http://localhost:8080/style-atual-webservice/vendedor/pagination?firsResult=1&maxResult=3]. Tambem informando que a requisicao vai ser via GET
-	public @ResponseBody List<Vendedor> getByPagination(@RequestParam("firsResult") Integer firsResult, 
+	public ResponseEntity<List<Vendedor>> getByPagination(@RequestParam("firsResult") Integer firsResult, 
 			@RequestParam("maxResult") Integer maxResult) {//@ResponseBody para informar que o retorno vai em SJon no Body. @RequestParam Parametros embutido na requisicao sem criar outra requisicao REST
-		return vendedorService.getByPagination(firsResult, maxResult);
+		return new ResponseEntity<List<Vendedor>>(vendedorService.getByPagination(firsResult, maxResult), HttpStatus.OK);
 	}
 
 	@Override
 	@RequestMapping(value = "", method = RequestMethod.GET)//Mapiamneto Rest para o Spring poder encontrar o metodo no certo com parametro na requisicao[http://localhost:8080/style-atual-webservice/vendedor?name=andre]. Tambem informando que a requisicao vai ser via GET
-	public @ResponseBody List<Vendedor> getByName(@RequestParam("name") String name) {//@ResponseBody para informar que o retorno vai em SJon no Body. @RequestParam Parametro embutido na requisicao sem criar outra requisicao REST
-		return vendedorService.getByName(name);
+	public ResponseEntity<List<Vendedor>> getByName(@RequestParam("name") String name) {//@ResponseBody para informar que o retorno vai em SJon no Body. @RequestParam Parametro embutido na requisicao sem criar outra requisicao REST
+		return new ResponseEntity<List<Vendedor>>(vendedorService.getByName(name), HttpStatus.OK);
 	}
 	
 	@Override
 	@RequestMapping(value = "/getAll", method = RequestMethod.GET)//Mapiamneto Rest para o Spring poder encontrar o metodo no certo[http://localhost:8080/style-atual-webservice/vendedor/getAll]. Tambem informando que a requisicao vai ser via GET
-	public @ResponseBody List<Vendedor> getAll() {//@ResponseBody para informar que o retorno vai em SJon no Body
-		List<Vendedor> vendedores = vendedorService.getAll();
-		return vendedores;
+	
+	public ResponseEntity<List<Vendedor>> getAll() {//@ResponseBody para informar que o retorno vai em SJon no Body
+		return new ResponseEntity<List<Vendedor>>(vendedorService.getAll(), HttpStatus.OK);
 	}
 }
